b24a321b0fe0d486596b452fd1a308f10a245eb2
krishna <mail.krishnamoorthyc@gmail.com>
2022-10-31 12:38:19 +0800

    first version

--- src/main/java/com/scb/gcp/GetBucketMetadata.java
+++ src/main/java/com/scb/gcp/GetBucketMetadata.java
@@ -1,23 +1,63 @@
 package com.scb.gcp;
 
-import org.apache.log4j.Logger;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.BucketInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+//import org.apache.log4j.LogManager;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.*;
+
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Map;
 
 public class GetBucketMetadata {
 
-
+    static String  path = "C:\\Users\\Krishna\\Downloads\\gcs";
 
     public static void main(String[] args) {
         getBucketMetadata("sc-test-360609","sc-test-merklesg");
 
-        System.out.println(GetBucketMetadata.class.getName());
+        //listBucket("sc-test-360609","sc-test-merklesg");
+
+        //System.out.println(GetBucketMetadata.class.getName());
     }
 
+    public static void listBucket(String projectId, String bucketName) {
+        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+        Page<Bucket> buckets = storage.list();
 
+        for (Bucket bucket : buckets.iterateAll()) {
+            System.out.println(bucket.getName());
+
+        }
+    }
+
+    public static void downloadObject(
+            String projectId, String bucketName, String objectName, String destFilePath) {
+        // The ID of your GCP project
+        // String projectId = "your-project-id";
+
+        // The ID of your GCS bucket
+        // String bucketName = "your-unique-bucket-name";
+
+        // The ID of your GCS object
+        // String objectName = "your-object-name";
+
+        // The path to which the file should be downloaded
+        // String destFilePath = "/local/path/to/file.txt";
+
+        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+
+        Blob blob = storage.get(BlobId.of(bucketName, objectName));
+        blob.downloadTo(Paths.get(destFilePath + "/" + objectName ));
+        //blob.
+
+        System.out.println(
+                "Downloaded object "
+                        + objectName
+                        + " from bucket name "
+                        + bucketName
+                        + " to "
+                        + destFilePath);
+    }
 
     public static void getBucketMetadata(String projectId, String bucketName) {
         // The ID of your GCP project
@@ -32,9 +72,15 @@ public class GetBucketMetadata {
         Bucket bucket =
                 storage.get(bucketName, Storage.BucketGetOption.fields(Storage.BucketField.values()));
 
+        for (Iterator<Blob> it = bucket.list().getValues().iterator(); it.hasNext(); ) {
+            Blob blb = it.next();
+            System.out.println("Listing bucket content: " + blb.getName());
+            downloadObject(projectId,bucketName,blb.getName(),path);
+        }
 
-        System.out.println("Listing bucket content: " + bucket.list().getValues().toString());
+        //System.out.println("Listing bucket content: " + bucket.list().getValues().iterator());
         // Print bucket metadata
+        /*
         System.out.println("BucketName: " + bucket.getName());
         System.out.println("DefaultEventBasedHold: " + bucket.getDefaultEventBasedHold());
         System.out.println("DefaultKmsKeyName: " + bucket.getDefaultKmsKeyName());
@@ -64,6 +110,8 @@ public class GetBucketMetadata {
                 System.out.println(rule);
             }
         }
+
+         */
     }
 }
 // [END storage_get_bucket_metadata]
